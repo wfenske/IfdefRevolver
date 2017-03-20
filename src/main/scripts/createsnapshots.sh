2b2e941552fd2b24dc1b06dd527d15d37ad26a68
wfenske <wcomm@gmx.de>
2017-03-20 17:25:28 +0100

    Also build an archive with all the dependencies included

--- src/main/scripts/createsnapshots.sh
+++ src/main/scripts/createsnapshots.sh
@@ -17,6 +17,7 @@ for o in "$@"; do
 done
 
 main_class=de.ovgu.skunk.bugs.createsnapshots.main.CreateSnapshots
+#CP=$HOME/.m2/repository/de/ovgu/skunk/IfdefRevolver/1.0-SNAPSHOT/IfdefRevolver-1.0-SNAPSHOT-jar-with-dependencies.jar
 if [ -n "$o_smellconfigs_dir" ]
 then
     exec java ${o_jvm} -cp "$CP" ${main_class} $o_smellconfigs_dir "$@"
