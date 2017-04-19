e0033265dbf396077a160e8d74786dde44807e41
Wolfram Fenske <wfenske@ovgu.de>
2017-04-19 14:01:41 +0200

    Even more memory for listing changed functions

--- src/main/scripts/lschfuncs.sh
+++ src/main/scripts/lschfuncs.sh
@@ -7,7 +7,7 @@ me_dir=$(dirname -- "${real_me}")
 #( IFS=:; for e in $CP; do logcfg=${e}/log4j.xml; test -e "$logcfg" && echo $logcfg; done )
 
 # Increase memory
-o_jvm="-Xms2g -Xmx2g"
+o_jvm="-Xms1g -Xmx4g"
 #o_jvm="-Xms32m -Xmx32m"
 
 exec java ${o_jvm} -cp "${CP:?}" de.ovgu.ifdefrevolver.commitanalysis.ListChangedFunctions "$@"
