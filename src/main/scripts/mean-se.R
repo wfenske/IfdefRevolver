cf50e5406e570c7ce73c7ab7e1f7f1515213f2fe
wfenske <wcomm@gmx.de>
2017-06-20 17:47:10 +0200

    New script to compute mean and standard error for descriptive statistics

---
+++ src/main/scripts/mean-se.R
@@ -0,0 +1,74 @@
+#!/usr/bin/env Rscript
+
+library(optparse)
+
+printf  <- function(...) cat(sprintf(...), sep='', file=stdout())
+eprintf <- function(...) cat(sprintf(...), sep='', file=stderr())
+
+options <- list(
+    make_option(c("-d", "--delimiter"),
+                default=",",
+                help="Column delimiter, e.g. `,' or `;'. [default: %default]")
+  , make_option(c("-c", "--column"),
+                default=NULL,
+                help="Name of the column to consider.")
+  , make_option("--digits",
+                default=2,
+                help="Number of decimal digits to round to. [default: %default]")
+  , make_option(c("-H", "--no-header"),
+                dest="noHeader",
+                default=FALSE,
+                action="store_true",
+                help="Omit the CSV header.")
+)
+
+args <- parse_args(OptionParser(description = "Compute the mean and standard error of the values in a named column of a CSV file. If no file is given, input is read from stdin.",
+                                usage = "%prog [options] [file]",
+                                option_list=options),
+                   positional_arguments = c(0,1))
+opts <- args$options
+
+if (length(args$args) == 0) {
+    inputFile <- file("stdin")
+    open(inputFile)
+} else if (length(args$args) == 1) {
+    inputFile <- args$args[1]
+} else {
+    eprintf("ERROR Invalid number of input files. Expected at most one, got `%d'\n", length(args$args))
+    quit(status=1)
+}
+
+#cat(paste("inputFn=", inputFn, "; outputFn=", outputFn, sep=""))
+#cat("\n")
+
+#stop("")
+
+#inputFn <- commandArgs(trailingOnly=TRUE)[1] # should be overview.csv or overviewSize.csv
+
+data <- read.csv(file=inputFile, head=TRUE, sep=opts$delimiter)
+
+if (is.null(opts$column)) {
+    opts$column <- colnames(data)[[1]]
+    eprintf("WARN: No column name specified. Using first column, %s.\n", opts$column)
+}
+
+customRound <- function(x) { round(x, digits=opts$digits) }
+## Cf. https://stackoverflow.com/questions/2676554/in-r-how-to-find-the-standard-error-of-the-mean
+stdErr <- function(x) sd(x)/sqrt(length(x))
+
+values <- data[[opts$column]]
+
+avgVal <- mean(values)
+se <- stdErr(values)
+rAvgVal <- customRound(avgVal)
+rSe <- customRound(se)
+
+##eprintf("DEBUG %s\n", paste(values, collapse=", "))
+
+### Print the header
+if (!opts$noHeader) {
+    printf("mean(%s)%sse(%s)\n", opts$column, opts$delimiter, opts$column)
+}
+
+### Print the actual values
+printf("%.*f,%.*f\n", opts$digits, rAvgVal, opts$digits, rSe)
