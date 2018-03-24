b068d7d50430b9ad67842370e08c82da65993ca2
Wolfram Fenske <wfenske@ovgu.de>
2018-03-24 18:07:33 +0100

    Added new main program script

---
+++ src/main/scripts/lsmodage.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+real_me=$(realpath -- "$0")
+me_dir=$(dirname -- "${real_me}")
+. "${me_dir}"/setup_classpath.sh || exit $?
+
+#( IFS=:; for e in $CP; do logcfg=${e}/log4j.xml; test -e "$logcfg" && echo $logcfg; done )
+
+# Increase memory
+o_jvm="-Xms1g -Xmx4g"
+#o_jvm="-Xms32m -Xmx32m"
+
+exec java ${o_jvm} -cp "${CP:?}" de.ovgu.ifdefrevolver.commitanalysis.AddChangeDistances "$@"
