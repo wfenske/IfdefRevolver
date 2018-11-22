49893a3c76e77c5757251b8a555640cc756eec9a
Wolfram Fenske <wfenske@ovgu.de>
2018-11-22 08:02:35 +0100

    x

--- src/main/java/de/ovgu/ifdefrevolver/commitanalysis/branchtraversal/SnapshotCreatingCommitWalker.java
+++ src/main/java/de/ovgu/ifdefrevolver/commitanalysis/branchtraversal/SnapshotCreatingCommitWalker.java
@@ -3,12 +3,24 @@ package de.ovgu.ifdefrevolver.commitanalysis.branchtraversal;
 import de.ovgu.ifdefrevolver.bugs.minecommits.CommitsDistanceDb;
 
 public class SnapshotCreatingCommitWalker extends AbstractCommitWalker {
+    private int commitIndex;
+    private int snapshotIndex;
+    private final int snapshotSize;
+
     public SnapshotCreatingCommitWalker(CommitsDistanceDb commitsDistanceDb) {
         super(commitsDistanceDb);
+        this.snapshotSize = 100;
+    }
+
+    @Override
+    public void processCommits() {
+        this.commitIndex = 0;
+        this.snapshotIndex = 0;
+        super.processCommits();
     }
 
     @Override
     protected void processCurrentCommit() {
-
+        this.commitIndex++;
     }
 }
